de24d08bd5ed1a929047366a16f74ebb8ea29528
cmwslw <cwalker32@gmail.com>
2015-02-17 23:47:37 -0500

    Able to output useful CSV files.

--- travel_hamster.py
+++ travel_hamster.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 import json
 import itertools
 import urllib2
@@ -9,8 +10,20 @@ class Hamster(object):
     def __init__(self, plan):
         self.plan = plan
 
+    def parse_duration(self, dur):
+        hours = 0
+        for part in dur.split(' '):
+            if part.endswith('hrs'):
+                hours += float(part[:-3])
+            elif part.endswith('min'):
+                hours += float(part[:-3])/60.0
+            else:
+                raise ValueError
+        return hours
+
     def parse_r2r(self, p):
         uri = 'http://free.rome2rio.com/s/{from}/{to}'.format(**p)
+        uri += '?dates={leave_date}/{return_date}'.format(**p)
         page = urllib2.urlopen(uri)
         soup = BeautifulSoup(page.read())
         itins = soup.findAll('li', {'class': 'itinerary'})
@@ -24,7 +37,7 @@ class Hamster(object):
                 if cl == 'itinerary-title':
                     data['mode_desc'] = dd.text
                 elif cl == 'itinerary-time':
-                    data['travel_time'] = dd.text
+                    data['travel_hours'] = self.parse_duration(dd.text)
                 elif cl == 'itinerary-price':
                     parts = dd.text.split(u'\xa0-\xa0')
                     data['travel_price_min'] = parts[0]
@@ -38,23 +51,25 @@ class Hamster(object):
             options.append(data)
         return options
 
-    def print_options(self):
+    def gen_options(self):
         for dest in plan['to']:
-            leaveplan = dict(plan)
-            returnplan = dict(plan)
-            leaveplan['from'] = leaveplan['home']
-            leaveplan['to'] = dest
-            returnplan['from'] = dest
-            returnplan['to'] = returnplan['home']
-            leave_r2r_options = self.parse_r2r(leaveplan)
-            return_r2r_options = self.parse_r2r(returnplan)
-            for combo in itertools.product(leave_r2r_options, return_r2r_options):
-                rowdata = dict(leaveplan)
-                for prefix, opt in [('leave_', combo[0]), ('return_', combo[1])]:
-                    for key, value in opt.iteritems():
-                        rowdata[prefix+key] = value
-                print rowdata
-            #print currplan
+            currplan = dict(plan)
+            currplan['from'] = plan['home']
+            currplan['to'] = dest
+            r2r_options = self.parse_r2r(currplan)
+            for opt in r2r_options:
+                rowdata = dict(currplan)
+                rowdata.update(opt)
+                rowdata['has_car'] = rowdata['modes'].endswith('car')
+                yield rowdata
+
+    def print_options(self):
+        cols = ['to', 'mode_desc', 'modes', 'travel_hours', 'travel_price_min', 'travel_price_max', 'leave_date', 'return_date', 'has_car', 'source']
+        writer = csv.DictWriter(sys.stdout, cols, extrasaction='ignore')
+        writer.writeheader()
+
+        for opt in self.gen_options():
+            writer.writerow(opt)
 
 
 plan = json.load(open(sys.argv[1]))
